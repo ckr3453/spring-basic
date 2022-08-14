4d4302f0d45ede095eb19015285a26fce28e8d91
ckr3453 <ckr3453@gmail.com>
2022-08-14 14:37:09 +0900

    modified "빈 등록을 통한 configuration"

--- src/main/java/com/inflearn/demo/AppConfig.java
+++ src/main/java/com/inflearn/demo/AppConfig.java
@@ -2,29 +2,36 @@ package com.inflearn.demo;
 
 import com.inflearn.demo.discount.DiscountPolicy;
 import com.inflearn.demo.discount.FixDiscountPolicy;
+import com.inflearn.demo.discount.RateDiscountPolicy;
 import com.inflearn.demo.member.MemberService;
 import com.inflearn.demo.member.MemberServiceImpl;
 import com.inflearn.demo.member.MemoryMemberRepository;
 import com.inflearn.demo.order.OrderService;
 import com.inflearn.demo.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 
--- src/main/java/com/inflearn/demo/OrderApp.java
+++ src/main/java/com/inflearn/demo/OrderApp.java
@@ -4,15 +4,22 @@ import com.inflearn.demo.member.*;
 import com.inflearn.demo.order.Order;
 import com.inflearn.demo.order.OrderService;
 import com.inflearn.demo.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
