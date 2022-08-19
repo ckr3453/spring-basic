537d311cddffef73c23ce83afd972a3b6ee59219
ckr3453 <ckr3453@gmail.com>
2022-08-19 17:38:24 +0900

    modified "싱글톤 확인을 위한 로그출력"

--- src/main/java/com/inflearn/demo/AppConfig.java
+++ src/main/java/com/inflearn/demo/AppConfig.java
@@ -14,17 +14,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService 호출 시 new MemoryMemberRepository()를 호출하게 됨.
+    // @Bean orderService 호출 시 new MemoryMemberRepository()를 호출하게 됨.
+    // 싱글톤이 깨지는것 처럼 보임 (new 키워드를 사용)
+
+    // 예상출력 (싱글톤 적용안됨)
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // 실제출력 (싱글톤 적용)
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
+
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
