7739b647a449d056c766fdbf881232e5468ecac5
ckr3453 <ckr3453@gmail.com>
2022-08-19 14:51:05 +0900

    create "스프링 설정정보 파일 - xml 테스트"

---
+++ src/test/java/com/inflearn/demo/xml/XmlAppContext.java
@@ -0,0 +1,23 @@
+package com.inflearn.demo.xml;
+
+import com.inflearn.demo.member.MemberService;
+import com.inflearn.demo.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    @Test
+    @DisplayName("xml 기반 스프링 설정정보 파일을 통한 빈 등록")
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
