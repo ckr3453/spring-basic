65f70edeb2ab3146e87fbe92133468c516eca6ca
ckr3453 <ckr3453@gmail.com>
2022-08-22 18:33:22 +0900

    create "퓨어 자바코드 테스트"

---
+++ src/test/java/com/inflearn/demo/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.inflearn.demo.order;
+
+import com.inflearn.demo.discount.FixDiscountPolicy;
+import com.inflearn.demo.member.Grade;
+import com.inflearn.demo.member.Member;
+import com.inflearn.demo.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder(){
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
