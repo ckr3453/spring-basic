2368f72f9b5357529d10723d43c27682ad64a8b7
ckr3453 <ckr3453@gmail.com>
2022-08-22 16:58:45 +0900

    create "@ComponentScan 테스트"

---
+++ src/test/java/com/inflearn/demo/scan/AutoAppConfigTest.java
@@ -0,0 +1,22 @@
+package com.inflearn.demo.scan;
+
+import com.inflearn.demo.AutoAppConfig;
+import com.inflearn.demo.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AutoAppConfigTest {
+
+    @Test
+    @DisplayName("컴포넌트 스캔 테스트")
+    void basicScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService bean = ac.getBean(MemberService.class);
+        assertThat(bean).isInstanceOf(MemberService.class);
+    }
+}
