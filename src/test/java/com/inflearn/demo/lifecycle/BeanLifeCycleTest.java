75b2968bd2adf3c99f574bd0ed362fd05aef4e8f
ckr3453 <ckr3453@gmail.com>
2022-08-24 15:46:29 +0900

    create "빈 생명주기 테스트"

---
+++ src/test/java/com/inflearn/demo/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,33 @@
+package com.inflearn.demo.lifecycle;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+public class BeanLifeCycleTest {
+
+    @Test
+    @DisplayName("빈 생명주기 테스트")
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://demo.dev");
+            return networkClient;
+        }
+
+    }
+}
---
+++ src/test/java/com/inflearn/demo/lifecycle/NetworkClient.java
@@ -0,0 +1,50 @@
+package com.inflearn.demo.lifecycle;
+
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close: " + url);
+    }
+
+    // 의존관계 주입이 끝나면 호출
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    // 빈 소멸 직전에 호출
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
+
